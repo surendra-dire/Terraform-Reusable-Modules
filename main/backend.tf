844c3ee97b013876df5e57df4bc0e5f039369f91
Surendra Dire <83612581+surendra-dire@users.noreply.github.com>
2025-06-12 08:53:44 +0200

    Delete main/backend.tf

--- main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-bucket-1-surendra"    # Replace with your actual bucket name
-    key            = "envs/dev/terraform.tfstate"   # Path to your state file in the bucket
-    region         = "eu-west-1"                    # AWS region where the bucket and table exist
-    dynamodb_table = "tfstate-lock"                 # DynamoDB table for state locking
-    encrypt        = true                           # Encrypt the state file at rest
-  }
-}
