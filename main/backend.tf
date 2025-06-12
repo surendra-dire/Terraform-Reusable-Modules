24a9b0e38de968df2c3ffb31061dd5b0d9212eb7
Surendra Dire <83612581+surendra-dire@users.noreply.github.com>
2025-06-12 11:57:57 +0200

    Update backend.tf

--- main/backend.tf
+++ main/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "tfstate-bucket-1-surendra"    # Replace with your actual bucket name
     key            = "envs/dev/terraform.tfstate"   # Path to your state file in the bucket
     region         = "eu-west-1"                    # AWS region where the bucket and table exist
-    dynamodb_table = "tfstate-lock"                 # DynamoDB table for state locking
+    #dynamodb_table = "tfstate-lock"                # DynamoDB table for state locking
+    use_lockfile   = true
     encrypt        = true                           # Encrypt the state file at rest
   }
 }
